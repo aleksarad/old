ef987df63bf8c15a982c8cfd801370e75aaed915
aleksarad <52298236+aleksarad@users.noreply.github.com>
2019-09-17 13:40:04 -0400

    Update portf.js
    updated all JS to jQuery

--- portf.js
+++ portf.js
@@ -1,17 +1,6 @@
-var darkToggle = document.getElementById("darkToggle");
-
-var body = document.querySelector("body");
-var nav = document.querySelector("nav");
-var link1 = document.getElementsByTagName("a")[0];
-var link2 = document.getElementsByTagName("a")[1];
-var mail = document.getElementById("emailicon");
-
-
-
-darkToggle.addEventListener("click", function()
-{body.classList.toggle("dark");
-nav.classList.toggle("darknav");
-link1.classList.toggle("whitenavlink");
-link2.classList.toggle("whitenavlink");
-mail.classList.toggle("mailicon");
+$("#darkToggle").click(function(){
+   $("body").toggleClass("dark"); 
+   $("nav").toggleClass("darknav");
+   $(".navlink").toggleClass("whitenavlink");
+   $("#emailicon").toggleClass("mailicon");
 })
